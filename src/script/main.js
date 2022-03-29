0147a195ae5bb2a845165eac724c897c5d4d22f1
Hermione Allognon <allognonhermione@gmail.com>
2022-03-29 19:49:51 +0200

    change api url

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,4 @@
-const url ="https://dummyjson.com/products/category/smartphones/";
+const url ="https://fakestoreapi.com/products";
 let items = document.querySelector(".items");
 
 
@@ -6,14 +6,14 @@ let items = document.querySelector(".items");
 
 fetch(url).then((response)=>response.json())
 .then((data)=>{
-    console.log(data.products)
-    data.products.forEach(product => {
+    console.log(data)
+    data.forEach(product => {
         console.log(product)
         let div = document.createElement("div");
         div.className="items__item";
         items.append(div)
         div.innerHTML = `
-            <img src=${product.images[0]} class="items__img"/>
+            <img src=${product.image} class="items__img"/>
             <p class="items__title">${product.title}</p>
             <p class="items__price">$${product.price}</p> `
     });
